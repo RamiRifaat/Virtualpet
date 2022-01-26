c104ec69300f8baefd317d0cb3ab6e27522bbd40
RamiRifaat <rrifaat@eastsideprep.org>
2022-01-25 22:22:04 -0800

    commiting to github

---
+++ js/pet.js
@@ -0,0 +1,15 @@
+let mood = 0;
+let belly = 0;
+
+
+function feedPet() {
+    console.log("feeding pet....");
+}
+
+function fanPet() {
+    console.log("fanning pet....");
+}
+
+function playFetch() {
+    console.log("Fecth pet....");
+}
